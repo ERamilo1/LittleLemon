bbb0fffd0f0fd3105ab6b2eb5b19e52b237c355c
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-11-20 18:16:53 -0500

    Created Menu and Booking models. Redigstered them in the admin.py file. Performed migrations.

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guests = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
