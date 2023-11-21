8714d683176bf6caf688aa50d919cc102afa5fa4
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-11-20 21:40:38 -0500

    Added rest_framework to installed apps in the settings file. Created 2 views for menu items from the Menu model. Created a serializers file and made a serializer for the Menu model. Added url endpoints for the menu item views.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
